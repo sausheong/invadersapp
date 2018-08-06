56010e3f15690ce983372698064a726344990a2b
sausheong <sausheong@gmail.com>
2018-08-07 07:52:14 +0800

    initial commit

---
+++ invaders.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"math/rand"
+	"time"
+
+	"github.com/disintegration/gift"
+)
+
+var aliensPerRow = 8
+var aliensStartCol = 100
+var alienSize = 30
+var bombProbability = 0.005
+var bombSpeed = 10
+
+// sprites
+var src image.Image
+var background image.Image
+var cannonSprite = image.Rect(20, 47, 38, 59)
+var cannonExplode = image.Rect(0, 47, 16, 57)
+var alien1Sprite = image.Rect(0, 0, 20, 14)
+var alien1aSprite = image.Rect(20, 0, 40, 14)
+var alien2Sprite = image.Rect(0, 14, 20, 26)
+var alien2aSprite = image.Rect(20, 14, 40, 26)
+var alien3Sprite = image.Rect(0, 27, 20, 40)
+var alien3aSprite = image.Rect(20, 27, 40, 40)
+var alienExplode = image.Rect(0, 60, 16, 68)
+var beamSprite = image.Rect(20, 60, 22, 65)
+var bombSprite = image.Rect(0, 70, 10, 79)
+
+// Sprite represents a sprite in the game
+type Sprite struct {
+	size     image.Rectangle // the sprite size
+	Filter   *gift.GIFT      // normal filter used to draw the sprite
+	FilterA  *gift.GIFT      // alternate filter used to draw the sprite
+	FilterE  *gift.GIFT      // exploded filter used to draw the sprite
+	Position image.Point     // top left position of the sprite
+	Status   bool            // alive or dead
+	Points   int             // number of points if destroyed
+}
+
+// sprite for laser cannon
+var laserCannon = Sprite{
+	size:     cannonSprite,
+	Filter:   gift.New(gift.Crop(cannonSprite)),
+	FilterE:  gift.New(gift.Crop(cannonExplode)),
+	Position: image.Pt(50, 250),
+	Status:   true,
+}
+
+// sprite for the laser beam
+var beam = Sprite{
+	size:     beamSprite,
+	Filter:   gift.New(gift.Crop(beamSprite)),
+	Position: image.Pt(laserCannon.Position.X+7, 250),
+	Status:   false,
+}
+
+// used for creating alien sprites
+func createAlien(x, y int, sprite, alt image.Rectangle, points int) (s Sprite) {
+	s = Sprite{
+		size:     sprite,
+		Filter:   gift.New(gift.Crop(sprite)),
+		FilterA:  gift.New(gift.Crop(alt)),
+		FilterE:  gift.New(gift.Crop(alienExplode)),
+		Position: image.Pt(x, y),
+		Status:   true,
+		Points:   points,
+	}
+	return
+}
+
+// generate frames for the game
+func generateFrames() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var aliens = []Sprite{}
+	var bombs = []Sprite{}
+	// game variables
+	loop := 0         // game loop
+	beamShot := false // the instance where the beam is shot
+
+	alienDirection := 1 // direction where alien is heading
+	score := 0          // number of points scored in the game so far
+
+	// populate the aliens
+	for i := aliensStartCol; i < aliensStartCol+(alienSize*aliensPerRow); i += alienSize {
+		aliens = append(aliens, createAlien(i, 30, alien1Sprite, alien1aSprite, 30))
+	}
+	for i := aliensStartCol; i < aliensStartCol+(30*aliensPerRow); i += alienSize {
+		aliens = append(aliens, createAlien(i, 55, alien2Sprite, alien2aSprite, 20))
+	}
+	for i := aliensStartCol; i < aliensStartCol+(30*aliensPerRow); i += alienSize {
+		aliens = append(aliens, createAlien(i, 80, alien3Sprite, alien3aSprite, 10))
+	}
+
+	// main game loop
+	for !gameOver {
+		// if any of the keyboard events are captured
+		select {
+		case ev := <-events:
+			// exit the game
+			if ev == "81" { // q
+				gameOver = true
+			}
+			if ev == "32" { // space bar
+				if beam.Status == false {
+					beamShot = true
+				}
+				playSound("shoot")
+			}
+			if ev == "39" { // right arrow key
+				laserCannon.Position.X += 10
+			}
+			if ev == "37" { // left arrow key
+				laserCannon.Position.X -= 10
+			}
+		default:
+		}
+
+		// create background
+		dst := image.NewRGBA(image.Rect(0, 0, windowWidth, windowHeight))
+		gift.New().Draw(dst, background)
+
+		// process aliens
+		for i := 0; i < len(aliens); i++ {
+			aliens[i].Position.X = aliens[i].Position.X + 3*alienDirection
+			if aliens[i].Status {
+				// if alien is hit by a laser beam
+				if collide(aliens[i], beam) {
+					// draw the explosion
+					aliens[i].FilterE.DrawAt(dst, src, image.Pt(aliens[i].Position.X, aliens[i].Position.Y), gift.OverOperator)
+					// alien dies, player scores points
+					aliens[i].Status = false
+					score += aliens[i].Points
+					// reset the laser beam
+					resetBeam()
+				} else {
+					// show alternating aliens
+					if loop%2 == 0 {
+						aliens[i].Filter.DrawAt(dst, src, image.Pt(aliens[i].Position.X, aliens[i].Position.Y), gift.OverOperator)
+					} else {
+						aliens[i].FilterA.DrawAt(dst, src, image.Pt(aliens[i].Position.X, aliens[i].Position.Y), gift.OverOperator)
+					}
+					// drop torpedoes
+					if rand.Float64() < bombProbability {
+						bombs = append(bombs, dropBomb(aliens[i]))
+					}
+				}
+			}
+		}
+
+		// draw bombs, if laser cannon is hit, game over
+		for i := 0; i < len(bombs); i++ {
+			bombs[i].Position.Y = bombs[i].Position.Y + bombSpeed
+			bombs[i].Filter.DrawAt(dst, src, image.Pt(bombs[i].Position.X, bombs[i].Position.Y), gift.OverOperator)
+			if collide(bombs[i], laserCannon) {
+				gameOver = true
+				laserCannon.FilterE.DrawAt(dst, src, image.Pt(laserCannon.Position.X, laserCannon.Position.Y), gift.OverOperator)
+			}
+		}
+		// draw the laser cannon unless it's been destroyed
+		if !gameOver {
+			laserCannon.Filter.DrawAt(dst, src, image.Pt(laserCannon.Position.X, laserCannon.Position.Y), gift.OverOperator)
+		}
+
+		// move the aliens back and forth
+		if aliens[0].Position.X < alienSize || aliens[aliensPerRow-1].Position.X > windowWidth-(2*alienSize) {
+			alienDirection = alienDirection * -1
+			for i := 0; i < len(aliens); i++ {
+				aliens[i].Position.Y = aliens[i].Position.Y + 10
+			}
+		}
+
+		// if the beam is shot, place the beam at start of the cannon
+		if beamShot {
+			beam.Position.X = laserCannon.Position.X + 7
+			beam.Status = true
+			beamShot = false
+		}
+
+		// keep drawing the beam as it moves every loop
+		if beam.Status {
+			beam.Filter.DrawAt(dst, src, image.Pt(beam.Position.X, beam.Position.Y), gift.OverOperator)
+			beam.Position.Y -= 10
+		}
+
+		// if the beam leaves the window reset it
+		if beam.Position.Y < 0 {
+			resetBeam()
+		}
+
+		// if the aliens reach the position of the cannon, it's game over!
+		if aliens[0].Position.Y > 180 {
+			gameOver = true
+		}
+		createFrame(dst)
+		// pause a bit before ending the game
+		if gameOver {
+			playSound("explosion")
+			time.Sleep(time.Second)
+		}
+		loop++
+	}
+
+	// show end screen and score
+	endScreen := getImage(dir + "/public/images/gameover.png").(*image.RGBA)
+	printLine(endScreen, 137, 220, fmt.Sprintf("Your score is %d", score), color.RGBA{255, 0, 0, 255})
+	printLine(endScreen, 104, 240, "Press 's' to play again", color.RGBA{255, 0, 0, 255})
+	printLine(endScreen, 137, 260, "Press 'q' to quit", color.RGBA{255, 0, 0, 255})
+
+	createFrame(endScreen)
+}
+
+// alien drops the bomb
+func dropBomb(alien Sprite) (torpedo Sprite) {
+	torpedo = Sprite{
+		size:     bombSprite,
+		Filter:   gift.New(gift.Crop(bombSprite)),
+		Position: image.Pt(alien.Position.X+7, alien.Position.Y),
+		Status:   true,
+	}
+	return
+}
+
+// resets the beam once it goes out of the frame
+func resetBeam() {
+	beam.Status = false
+	beam.Position.Y = 250
+}
+
+// checks if two sprites collide
+func collide(s1, s2 Sprite) bool {
+	spriteA := image.Rect(s1.Position.X, s1.Position.Y, s1.Position.X+s1.size.Dx(), s1.Position.Y+s1.size.Dy())
+	spriteB := image.Rect(s2.Position.X, s2.Position.Y, s2.Position.X+s1.size.Dx(), s2.Position.Y+s1.size.Dy())
+	if spriteA.Min.X < spriteB.Max.X && spriteA.Max.X > spriteB.Min.X &&
+		spriteA.Min.Y < spriteB.Max.Y && spriteA.Max.Y > spriteB.Min.Y {
+		return true
+	}
+	return false
+}
---
+++ main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"html/template"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"golang.org/x/image/font/inconsolata"
+
+	"github.com/faiface/beep/speaker"
+	"github.com/faiface/beep/wav"
+	"github.com/zserge/webview"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+var frame string       // game frames
+var dir string         // current directory
+var events chan string // keyboard events
+var gameOver = false   // end of game
+
+// width and height of the window
+var windowWidth, windowHeight = 400, 300
+
+// main function
+func main() {
+	// events is a channel of string events that come from the front end
+	events = make(chan string, 1000)
+	// getting the current directory to access resources
+	var err error
+	dir, err = filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	src = getImage(dir + "/public/images/sprites.png")
+	background = getImage(dir + "/public/images/bg.png")
+
+	// run the web server in a separate goroutine
+	go server()
+
+	// create a web view
+	err = webview.Open("Space Invaders", "http://127.0.0.1:12346/public/html/index.html", windowWidth, windowHeight, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// web server
+func server() {
+	mux := http.NewServeMux()
+	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir(dir+"/public"))))
+	mux.HandleFunc("/start", start)
+	mux.HandleFunc("/frame", getFrame)
+	mux.HandleFunc("/key", captureKeys)
+	server := &http.Server{
+		Addr:    "127.0.0.1:12346",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
+
+// start the game
+func start(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles(dir + "/public/html/invaders.html")
+	// start generating frames in a new goroutine
+	go generateFrames()
+	t.Execute(w, "")
+}
+
+// capture keyboard events
+func captureKeys(w http.ResponseWriter, r *http.Request) {
+	ev := r.FormValue("event")
+	// what to react to when the game is over
+	if gameOver {
+		if ev == "83" { // s
+			gameOver = false
+			go generateFrames()
+		}
+		if ev == "81" { // q
+			os.Exit(0)
+		}
+
+	} else {
+		events <- ev
+	}
+
+}
+
+// get the game frames
+func getFrame(w http.ResponseWriter, r *http.Request) {
+	str := "data:image/png;base64," + frame
+	w.Write([]byte(str))
+}
+
+// print a line of text to the image
+func printLine(img *image.RGBA, x, y int, label string, col color.RGBA) {
+	point := fixed.Point26_6{X: fixed.Int26_6(x * 64), Y: fixed.Int26_6(y * 64)}
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(col),
+		Face: inconsolata.Bold8x16,
+		Dot:  point,
+	}
+	d.DrawString(label)
+}
+
+// create a frame from the image
+func createFrame(img image.Image) {
+	var buf bytes.Buffer
+	png.Encode(&buf, img)
+	frame = base64.StdEncoding.EncodeToString(buf.Bytes())
+}
+
+// play a sound
+func playSound(name string) {
+	f, _ := os.Open(dir + "/public/sounds/" + name + ".wav")
+	s, format, _ := wav.Decode(f)
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/20))
+	speaker.Play(s)
+}
+
+// get an image from the file
+func getImage(filePath string) image.Image {
+	imgFile, err := os.Open(filePath)
+	defer imgFile.Close()
+	if err != nil {
+		fmt.Println("Cannot read file:", err)
+	}
+	img, _, err := image.Decode(imgFile)
+	if err != nil {
+		fmt.Println("Cannot decode file:", err)
+	}
+	return img
+}
